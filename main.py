d7cf2ff9f161915d3d435363fc3bae0aa05b89c8
Emmanuel Okafor <emmanuel.okafor4@mail.dcu.ie>
2025-09-10 12:28:29 +0100

    idk what to say anymore

---
+++ main.py
@@ -0,0 +1,11 @@
+import random
+
+quotes = [
+    "Keep going, you're doing great!",
+    "Done is better than perfect.",
+    "Code. Commit. Push. Repeat.",
+    "Every expert was once a beginner.",
+    "Small steps every day add up."
+]
+
+print(random.choice(quotes))
